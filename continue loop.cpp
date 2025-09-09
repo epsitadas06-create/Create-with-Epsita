63458e1d44baadce75bd44b01036509adef61eb5
Epsita Das <epsitadas06@gmail.com>
2025-09-09 19:53:59 +0530

    Create continue loop.cpp

---
+++ continue loop.cpp
@@ -0,0 +1,13 @@
+//this is epsita here
+// continue loop example
+#include <iostream>
+using namespace std; 
+int main () 
+{ 
+ for (int n=10; n>0; n--) { 
+ if (n==5) continue; 
+ cout << n << ", "; 
+ } 
+ cout << "FIRE!\n"; 
+ return 0; 
+}
